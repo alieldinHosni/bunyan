9bcb00b948f2ee193611c99713a752495a59018a
alieldinHosni <alieldinhosni@gmail.com>
2026-09-01 13:53:52 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* Offline cache. Bump CACHE when you change index.html. */
+const CACHE = "bunyan-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest",
+               "./icon.svg", "./icon-180.png", "./icon-512.png", "./logotype.svg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === "basic") {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
